9a462ff9f20e78f190dc58152d654345feb10275
JustinDB <justindbethea1995@gmail.com>
2023-01-23 16:47:03 -0500

    Perk Self-Destruct on load if owned

--- Assets/Scripts/PickUps/PerkPickup.cs
+++ Assets/Scripts/PickUps/PerkPickup.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class PerkPickup : MonoBehaviour
 {
     [SerializeField] SO_Perk perk;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < ScoreManager.instance.ownedList.Count; i++)
+        {
+            if (ScoreManager.instance.ownedList[i] == perk.ID)
+            {
+                Destroy(gameObject);
+                break;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
