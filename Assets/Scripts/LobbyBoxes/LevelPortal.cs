10f589c4a96969de68eebbc0bb20950bf8681ec7
JustinDB <justindbethea1995@gmail.com>
2023-02-14 18:49:31 -0500

    Icon, Portal Audio

--- Assets/Scripts/LobbyBoxes/LevelPortal.cs
+++ Assets/Scripts/LobbyBoxes/LevelPortal.cs
@@ -7,13 +7,20 @@ public class LevelPortal : MonoBehaviour
 {
 
     [SerializeField] LevelSelecter selecter;
+    [SerializeField] AudioSource audPlayer;
+    [SerializeField] AudioClip portalClip;
 
+
+    private void Start()
+    {
+        audPlayer.volume = SaveManager.instance.gameData.sfxVol;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && selecter.isOpen)
         {
-
+            audPlayer.PlayOneShot(portalClip);
             if (ScoreManager.instance.Scenes.Count > selecter.Index)
             {
                 //SceneManager.LoadScene(ScoreManager.instance.Scenes[selecter.Index]);
